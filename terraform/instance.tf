545b7be69ba2ca074a649771d6a716147ee34578
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-08-28 13:12:56 -0300

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -4,4 +4,5 @@ resource "aws_instance" "projeto3" {
   instance_type = "t2.micro"
   subnet_id     = "subnet-04ddf27ca8d47df64"
   key_name	    = "projeto3"
+  user_data     = yum -y update yum install epel-release -y yum install ansible git -y
 }
