5687e4025823a8e401f0ff34adbbc590410f2048
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-08-29 23:01:31 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-echo "Realizando teste de conectividade..."
-
-#Verifica se a instancia AWS está UP
-
 URL="http://www.google.com"
 SERVICE_PORT=8080
 
+echo "Realizando teste de conectividade..."
+
+#Verifica se a instancia AWS está UP
 #Falta - Verificar como pega a URL da instancia via terraform e salvar em variavel...
 #ping -qc5 $URL > /dev/null
 
@@ -28,7 +27,7 @@ STATUS_CODE=$(curl --write-out %{http_code} --silent --output /dev/null $URL:$SE
 #Verifica se a resposta do HTTP GET é igual a 200, caso verdadeiro a requisiço foi um sucesso.
 if [ $STATUS_CODE -eq 200 ]; then
 	echo -e "Teste de requisição....\033[0;32mOK\033[0m :: $(date +%F\ %T)"
-	echo "URL da aplicação $URL:$SERVICE_PORT"
+	echo "URL da aplicação http://$URL:$SERVICE_PORT"
 else
 	echo -e "Teste de requisição... \033[0;31mFAIL\033[0m :: $(date +%F\ %T)"
 	echo "Teste de requisição falhou, verifique o log de deploy da aplicação!"
