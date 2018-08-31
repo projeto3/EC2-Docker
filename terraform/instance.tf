a939093c19326c23694061ebcefcd98683910299
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-08-31 19:49:19 -0300

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -2,6 +2,7 @@
 resource "aws_instance" "projeto3" {
   ami           = "ami-6871a115"
   instance_type = "t2.micro"
+  key_name = "projeto3"
   subnet_id     = "subnet-04ddf27ca8d47df64"
   # user_data = "${file("${path.module}/user-data.sh")}"
 }
