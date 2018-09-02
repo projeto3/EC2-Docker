3500c02ce4551e02d44832aa79b032e166e860af
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-09-02 14:46:34 -0300

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "projeto3" {
   ami           = "ami-6871a115"
   instance_type = "t2.micro"
   key_name = "projeto3"
-  subnet_id     = "subnet-04ddf27ca8d47df64"
+  #subnet_id     = "subnet-04ddf27ca8d47df64"
   associate_public_ip_address	= "true"
   #aws_security_group = "sg-0b70b9d3a1ae3add6"
   
