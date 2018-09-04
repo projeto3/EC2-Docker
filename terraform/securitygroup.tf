0785593d13dbf7281b3786e238132ad0d83dac6a
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-09-03 22:42:49 -0300

    Update securitygroup.tf
    Add regra que Libera o echo request, e a porta 8080

--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -15,9 +15,21 @@ resource "aws_security_group" "allow-ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 8
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
     ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
