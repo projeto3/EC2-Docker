caf339fefafdebc127906a118cabd7c3f3c19dd3
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-09-03 21:52:44 -0300

    Update securitygroup.tf

--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -21,6 +21,12 @@ resource "aws_security_group" "allow-ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
   
 tags {
     Name = "allow-ssh"
