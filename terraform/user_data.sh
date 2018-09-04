9bcdeeafe185d979a8457ffd5fac5fb84e0b79ac
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-09-04 09:50:49 -0300

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -4,4 +4,5 @@ yum -y install epel-release
 wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -ivh epel-release-latest-7.noarch.rpm
 yum repolist
-yum -y --enablerepo=epel install ansible
+yum -y --enablerepo=epel install ansible python-pip python-wheel
+pip install boto3
