ca0d65954ef597d5660f7bbb4274f2fbd283278c
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-09-05 19:45:55 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #URL="http://www.google.com"
 aws_dns=$(cat terraform/aws_dns.txt)
+echo $aws_dns
+echo
 SERVICE_PORT=8080
 
 echo "Realizando teste de conectividade..."
@@ -11,24 +13,24 @@ echo "Realizando teste de conectividade..."
 
 echo
 
-if ! ping -qc2 $dns_aws > /dev/null; then
-	echo -e "$dns_aws: Status da instancia EC2 -> \033[0;31mOFFLINE\033[0m"
+if ! ping -qc2 $aws_dns > /dev/null; then
+	echo -e "$aws_dns: Status da instancia EC2 -> \033[0;31mOFFLINE\033[0m"
 	echo "O teste de conectividade falhou, verifique sua instancia!"
 	exit
 else 
-	echo -e "$dns_aws: Status da instancia EC2 -> \033[0;32ONLINE\033[0m"
+	echo -e "$aws_dns: Status da instancia EC2 -> \033[0;32ONLINE\033[0m"
 fi
 
 echo "Realizando teste de requisição (HTTP GET)"
 sleep 1
 #Envia uma requisição HTTP GET para o APP
 
-STATUS_CODE=$(curl --write-out %{http_code} --silent --output /dev/null $dns_aws:$SERVICE_PORT)
+STATUS_CODE=$(curl --write-out %{http_code} --silent --output /dev/null $aws_dns:$SERVICE_PORT)
 
 #Verifica se a resposta do HTTP GET é igual a 200, caso verdadeiro a requisiço foi um sucesso.
 if [ $STATUS_CODE -eq 200 ]; then
 	echo -e "Teste de requisição....\033[0;32mOK\033[0m :: $(date +%F\ %T)"
-	echo "URL da aplicação http://$dns_aws:$SERVICE_PORT"
+	echo "URL da aplicação http://$aws_dns:$SERVICE_PORT"
 else
 	echo -e "Teste de requisição... \033[0;31mFAIL\033[0m :: $(date +%F\ %T)"
 	echo "Teste de requisição falhou, verifique o log de deploy da aplicação!"
