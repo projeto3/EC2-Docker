e3b594c99ab3ccf40577905189b5f4d8f5990648
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-09-06 01:02:03 -0300

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -5,34 +5,3 @@
  variable "PATH_TO_PUBLIC_KEY" {
  default = "/etc/ssh/projeto.pub"
  }
-    variable "instance_name" {
-  description = "Used to populate the Name tag. This is done in main.tf"
-      nome = "${aws_instance.projeto3.public_dns}"
-}
-
-variable "instance_type" {}
-
-variable "subnet_id" {
-  description = "The VPC subnet the instance(s) will go in"
-}
-
-variable "ami_id" {
-  description = "The AMI to use"
-}
-
-variable "number_of_instances" {
-  description = "number of instances to make"
-  default = 1
-}
-
-variable "user_data" {
-  description = "The path to a file with user_data for the instances"
-}
-
-variable "tags" {
-  default = {
-    created_by = "terraform"
- }
-}
-
-
