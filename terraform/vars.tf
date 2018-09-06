ecaa331bfd9561f9571f6a945050133bde050c39
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-09-06 00:11:15 -0300

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -5,3 +5,14 @@
  variable "PATH_TO_PUBLIC_KEY" {
  default = "/etc/ssh/projeto.pub"
  }
+  variable "tags" {
+    description = "A map of tags to add to all resources"
+    default     = {
+      name = "${$BUILD_TAG}"
+      commit = "{$GIT_COMMIT}"
+      Build = "{$BUILD_DISPLAY_NAME}"
+    
+      }
+  }
+
+
