c72ac19370a46d15b28cd1d94592ad836ca1903a
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-09-08 08:28:10 -0300

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -2,14 +2,15 @@
 resource "aws_instance" "projeto3" {
   ami           = "ami-04681a1dbd79675a5"
   instance_type = "t2.micro"
+ #key_name = "projeto3"
   key_name = "${aws_key_pair.mykeypair.key_name}"
   subnet_id     = "${aws_subnet.main-public-1.id}"
   vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
   associate_public_ip_address	= "true"
   #aws_security_group = "sg-0b70b9d3a1ae3add6"
   
-  user_data = { "${file("${path.module}/user_data.sh")}"}
-  
+  user_data = "${file("${path.module}/user_data.sh")}"
+
 }
 output "aws_ip" {
     value = "${aws_instance.projeto3.public_ip}"
