8fdf17a7ba3aac843006a1cbdfabfd92e2276bc9
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-09-08 08:22:22 -0300

    Delete locals.tf

--- terraform/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  common_tags = "${map(
-    "Project", "openshift",
-//    "KubernetesCluster", "${var.cluster_id}",
-//    "kubernetes.io/cluster/${var.cluster_name}", "${var.cluster_id}"
-  )}"
-}
