e70d89ba2a4ba8a474b58fcec434bdc52762cc46
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-09-08 08:04:07 -0300

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -5,3 +5,13 @@
  variable "PATH_TO_PUBLIC_KEY" {
  default = "/etc/ssh/projeto.pub"
  }
+
+variable "tags" {
+  type = "map"
+  default = {
+    development = "bucket-dev"
+    staging = "bucket-for-staging"
+    preprod = "bucket-name-for-preprod"
+    production = "bucket-for-production"
+  }
+}
