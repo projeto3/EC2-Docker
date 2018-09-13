4c85595e864da0e7ef4adf8f8e67f620d303ac1f
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-09-12 21:20:01 -0300

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "projeto3" {
   ami           = "ami-04681a1dbd79675a5"
   instance_type = "t2.micro"
   key_name = "projeto3"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
+  public_key = "${file("projeto.pub")}"
   #key_name = "${aws_key_pair.mykeypair.key_name}"
   subnet_id     = "${aws_subnet.main-public-1.id}"
   vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
