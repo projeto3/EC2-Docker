60a3be9eabd4dd9fe7b1849d9c72779513d40be9
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-09-12 21:24:21 -0300

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "projeto3" {
   ami           = "ami-04681a1dbd79675a5"
   instance_type = "t2.micro"
   key_name = "projeto3"
-  public_key = "${file("projeto.pub")}"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDaxJuIbOf9l7pA45QAAWGbEL1ie0ljXbQkkfQBseIrv4iBRurSTTEQihS/+tXx2fksMmGGWWoasywuvRNrYIqyyp0ZkIBWYgl7VxZz8UzMIYsq+jHEEeUmMmYZ6IP+stJV8o2+HBpnjPULozoJu4ceUHDHMQiGxvxKszmJwwx2WxiDUZKXs3zStLpEjwol6oClp+P9rj3V6HlVTeGk+UHEhz+eGgcAslgWpCwrD/ZJ/bHrqSVfXlGe0vsubE7KoVtiFmc67G9Nsv92vNXlY8KeIeHph0Yq0niPHlRZe1UecQ8HR+7pmyo13xAb25ajFenG5gOETSHjwOMROT6Va//P"
   #key_name = "${aws_key_pair.mykeypair.key_name}"
   subnet_id     = "${aws_subnet.main-public-1.id}"
   vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
