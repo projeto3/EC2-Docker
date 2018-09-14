7227a2f3b4a4333b0204d33fc535b45dcf575f1f
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-09-14 19:30:40 -0300

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "projeto3" {
-  ami           = "ami-04681a1dbd79675a5"
+  ami           = "ami-6871a115"
   instance_type = "t2.micro"
   private_key = "${file("${path.module}/projeto3.pem")}"
   key_name = "projeto3"
