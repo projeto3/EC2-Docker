51c06593b4ba2d51012131652abe2d88d4ea81a1
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-09-14 17:41:03 -0300

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,7 +1,9 @@
   variable "PATH_TO_PRIVATE_KEY" {
+  name = "privada"
   default = "/etc/ssh/projeto3.pem"
 }
 
  variable "public_key_path" {
+   name = "publica"
  default = "/home/ec2-user/.ssh/id_rsa"
  }
