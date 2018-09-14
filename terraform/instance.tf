9eb8b7476e64b43e57835a320ce2d9b3da8c5ef7
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-09-14 18:01:52 -0300

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -2,7 +2,8 @@
 resource "aws_instance" "projeto3" {
   ami           = "ami-04681a1dbd79675a5"
   instance_type = "t2.micro"
-  key_name = "${var.private_key_path)}"
+  key_name = "projeto3"
+  #key_name = "${var.private_key_path)}"
   public_key = "${var.public_key_path}"
   #key_name = "${aws_key_pair.mykeypair.key_name}"
   subnet_id     = "${aws_subnet.main-public-1.id}"
