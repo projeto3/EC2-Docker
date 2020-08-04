2390720a82f9cc8f50217b8467a8e7e5a11dd233
projeto3 <42242436+projeto3@users.noreply.github.com>
2020-08-03 21:31:55 -0300

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "main" {
     enable_dns_support = "true"
     enable_dns_hostnames = "true"
     enable_classiclink = "false"
-    tags {
+    tags = {
         Name = "main"
     }
 }
@@ -16,7 +16,7 @@ resource "aws_subnet" "main-public-1" {
     map_public_ip_on_launch = "true"
     availability_zone = "us-east-1a"
 
-    tags {
+    tags = {
         Name = "main-public-1"
     }
 }
@@ -28,7 +28,7 @@ resource "aws_subnet" "main-private-1" {
     map_public_ip_on_launch = "false"
     availability_zone = "us-east-1c"
 
-    tags {
+    tags = {
         Name = "main-private-1"
     }
 }
@@ -38,7 +38,7 @@ resource "aws_subnet" "main-private-1" {
 resource "aws_internet_gateway" "main-gw" {
     vpc_id = "${aws_vpc.main.id}"
 
-    tags {
+    tags = {
         Name = "main"
     }
 }
@@ -51,7 +51,7 @@ resource "aws_route_table" "main-public" {
         gateway_id = "${aws_internet_gateway.main-gw.id}"
     }
 
-    tags {
+    tags = {
         Name = "main-public-1"
     }
 }
